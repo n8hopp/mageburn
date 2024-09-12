1f060362ceeb2548a7ba404841100a9604270398
clarkm25 <clarkm25@up.edu>
2024-09-12 15:37:21 -0700

    Cleanup of charged shot as well as bear trap
    - Fixed blurring issue on charged shot as well as the spawning location by changing the number of h_frames.
    
    - Added free set of trap assets
    
    - Added bear trap asset to the Archer's bear trap as well as animations for it.

--- scripts/traps/bear_trap.gd
+++ scripts/traps/bear_trap.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 var start_position = Vector2.ZERO
+var single_use = false
+@onready var _animation = $AnimationPlayer
+@onready var _trap_circle = $TrapCircle
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,10 +15,22 @@ func _process(delta: float):
 	pass
 
 func _on_body_entered(body):
-	if body.is_in_group("hurtbox"):
-		body.take_damage()
-		queue_free()
-		
+	if single_use == false:
+		if body.is_in_group("hurtbox"):
+			single_use = true
+			_animation.play("close")
+			_trap_circle.show()
+			$TrapTimer.start()
+			activated_trap(body)
+			
+
+func activated_trap(body: Node2D):
+	body.velocity = Vector2(0,0)
+
 
 func _on_timer_timeout():
+	queue_free()
+
+
+func _on_trap_timer_timeout():
 	queue_free()
