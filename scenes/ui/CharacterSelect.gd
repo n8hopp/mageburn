858d59b6ba9608dd44a5b94245e878e6ae6c045e
PennyS8 <silliman25@up.edu>
2024-09-15 12:59:27 -0700

    Scaled character select UI properly
    Also changed the UI images visibility to "nearest"

--- scenes/ui/CharacterSelect.gd
+++ scenes/ui/CharacterSelect.gd
@@ -1,13 +1,11 @@
-extends Node2D
+extends PanelContainer
 
 
-# Called when the node enters the scene tree for the first time.
-
-func _on_paladin_class_button_pressed():
+func _on_paladin_button_pressed():
 	#character = "Paladin"
 	get_tree().change_scene_to_file("res://scenes/level/level.tscn")
 
 
-func _on_ranger_class_button_pressed():
+func _on_ranger_button_pressed():
 	#character = "Ranger"
-	get_tree().change_scene_to_file("res://scenes/level/level.tscn")
+	get_tree().change_scene_to_file("res://scenes/level/ArcherTestLevel.tscn")
