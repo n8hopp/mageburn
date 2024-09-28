89f862440775c58180e8d1058c7e97428f3f89b1
n8hopp <hopper26@up.edu>
2024-09-28 14:46:00 -0700

    Health indicator lasts longer than damage indicator

--- scripts/ui/number_popup.gd
+++ scripts/ui/number_popup.gd
@@ -10,17 +10,20 @@ enum NUMTYPE {DAMAGE, HEAL}
 var number
 
 func _ready():
+	var timelength : float
 	match numtype:
 		NUMTYPE.DAMAGE:
 			label.label_settings.font_color = damage_color
 			label.label_settings.outline_color = Color.BLACK
 			label.text = "-" + str(number)
+			timelength = 1
 		NUMTYPE.HEAL:
 			label.label_settings.font_color = heal_color
 			label.label_settings.outline_color = Color.BLACK
 			label.text = "+" + str(number)
+			timelength = 3
 			
 	var tween = create_tween().set_parallel(true)
-	tween.tween_property(label, "position", Vector2(0, -20), 1)
-	tween.tween_property(label, "modulate", Color.TRANSPARENT, 1)
+	tween.tween_property(label, "position", Vector2(0, -20), timelength)
+	tween.tween_property(label, "modulate", Color.TRANSPARENT, timelength)
 	tween.chain().tween_callback(queue_free)
