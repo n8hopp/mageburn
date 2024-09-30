30b5129f2c99db542dab7007d1e3cff31d5e5e17
HarryThVu <vuh25@up.edu>
2024-09-29 19:11:08 -0700

    Boar enemy fixed and in
    Bit of a difficult enemy but in

---
+++ scripts/boar/Hit.gd
@@ -0,0 +1,14 @@
+extends StateMachineState
+@export var parent : CharacterBody2D
+
+func on_physics_process(delta):
+	state_machine.animation_player.play("hurt")
+	
+	parent.velocity = parent.knockback
+	parent.move_and_slide()
+	
+	parent.knockback = lerp(parent.knockback, Vector2.ZERO, 0.2)
+	
+func on_animation_finished(_anim_name: StringName) -> void:
+	if _anim_name == "hurt":
+		state_machine.change_state("Path")
---
+++ scripts/boar/boar.gd
@@ -0,0 +1,60 @@
+extends CharacterBody2D
+
+@onready var _animation = $AnimationPlayer
+@onready var _navagent = $NavigationAgent2D
+@onready var _sprite = $Sprite2D
+
+@export var follow_target : CharacterBody2D
+@export var hitpoints = 6
+
+@export var movement_speed : float = 30.0
+@export var dead = false
+var pause_pathfinding = false
+var stun_frames = 0
+var knockback : Vector2 = Vector2.ZERO
+
+var xp_orb_drop = preload("res://scenes/basic_items/xp.tscn")
+var damage_number = preload("res://scenes/ui/number_popup.tscn")
+
+func take_damage(num):
+	if dead:
+		return
+	
+	#TODO: remove; only for testing damage purposes
+	num = 1
+	
+	hitpoints -= num
+	
+	var number_ui = damage_number.instantiate()
+	number_ui.numtype = number_ui.NUMTYPE.DAMAGE
+	number_ui.number = num
+	add_child(number_ui)
+	
+	if hitpoints <= 0:
+		$BoarMachine.change_state("Dead")
+		dead = true
+		set_collision_layer_value(3, false)
+		set_collision_layer_value(1, false)
+		set_collision_layer_value(4, false)
+		z_index = 0
+		instance_xp_orb()
+	else:
+		# base level hitstun stuff - prob change how we do this later
+		$BoarMachine.change_state("Hit")
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	if dead:
+		return
+
+func instance_xp_orb():
+	var xp_orb = xp_orb_drop.instantiate()
+	xp_orb.global_position = global_position
+	xp_orb.z_index = 1
+	get_tree().current_scene.add_child(xp_orb)
+
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("player_hurtbox"):
+		body.get_parent().take_hit(1)
